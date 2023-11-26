fed711ef764e49863361f8c8bf7ae26bd00102fd
DaanV2 <daanverstraten@hotmail.com>
2023-11-26 10:10:48 +0100

    Adding comments

--- lib/src/grid/square.rs
+++ lib/src/grid/square.rs
@@ -7,18 +7,21 @@ use super::{
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Square {
-    //The row index the square starts at
+    /// The row index the square starts at
     pub row: usize,
-    //The column index the square starts at
+    /// The column index the square starts at
     pub col: usize,
+    /// The grid of cells
     pub grid: [Cell; GRID_SIZE],
 }
 
 impl Square {
+    /// Creates a new square from the given row and column
     pub fn new(row: usize, col: usize, grid: [Cell; GRID_SIZE]) -> Self {
         Self { row, col, grid }
     }
 
+    /// Creates a new square from the given row and column
     pub fn from(row: usize, col: usize, grid: [Cell; GRID_SIZE]) -> Self {
         let row_offset = row - row % 3;
         let col_offset = col - col % 3;
@@ -26,27 +29,41 @@ impl Square {
         Square::new(row_offset, col_offset, grid)
     }
 
+    /// Gets the cell at the given index
     pub fn get_cell_at(&self, coord: Coord) -> Cell {
         let c = coord.offset(self.row, self.col);
         self.grid[get_index(c)]
     }
 
+    /// Gets the coord at the row and column
     pub fn get_coord_at(&self, row: usize, col: usize) -> Coord {
         Coord::new(self.row + row, self.col + col)
     }
 
+    /// Returns true if the column is within the square
     pub fn is_column_in_square(&self, col: usize) -> bool {
         col >= self.col && col < self.col + 3
     }
 
+    /// Returns true if the row is within the square
     pub fn is_row_in_square(&self, row: usize) -> bool {
         row >= self.row && row < self.row + 3
     }
 
+    /// Returns true if the coord is within the square
     pub fn is_coord_in_square(&self, coord: Coord) -> bool {
         self.is_row_in_square(coord.get_row()) && self.is_column_in_square(coord.get_col())
     }
 
+    /// Returns an iterator over the indices of the cells in the square
+    pub fn get_index(&self, coord: Coord) -> usize {
+        let row = coord.get_row() - self.row;
+        let col = coord.get_col() - self.col;
+
+        row * 3 + col
+    }
+
+    /// Returns an iterator over the indices of the cells in the square
     pub fn iter_square_coords() -> impl Iterator<Item = Coord> {
         static COORDS: [Coord; 9] = [
             Coord::new(0, 0),
